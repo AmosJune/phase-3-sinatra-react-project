ed6255f9650670977f5ab29f85b235f26bbd82ff
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-09-11 01:37:16 +0300

    create quotes table and its columns

--- db/migrate/20220910194114_create_quotes.rb
+++ db/migrate/20220910194114_create_quotes.rb
@@ -1,4 +1,8 @@
 class CreateQuotes < ActiveRecord::Migration[6.1]
   def change
+    create_table :quotes do |t|
+      t.string :title
+      t.string :description
+      t.string :author
   end
 end
